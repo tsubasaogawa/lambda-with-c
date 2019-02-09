e504235becfe0926806bcb4de56abc836cf39c0b
Tsubasa Ogawa <hurikake@gmail.com>
2019-02-09 09:50:16 +0000

    make it possible to use events

--- src/lambda_function.c
+++ src/lambda_function.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("hello world\n");
+  char buf[512];
+
+  while(1) {
+    /* Obtain from Stdin https://qiita.com/mpyw/items/aff12a6ff2c7726ed1d8 */
+    if(scanf("%511[^\n]%*[^\n]", buf) != 1) {
+      break;
+    }
+    /* Ignore linefeed */
+    scanf("%*c");
+
+    printf("%s\n", buf);
+  }
 
   return 0;
 }
